d8a81df07e03acd15843afbf2cd75c8b29a7ce9f
AntonSvenssonSpira <141945044+AntonSvenssonSpira@users.noreply.github.com>
2023-11-22 13:57:13 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# Skriv ett program som rekommenderar vilken SL biljett användaren ska köpa.
+# Programmet ska ha 2 inputs. Input 1 hur många gånger användaren ska åka. Input 2 under hur long period sak användaren göra resorna i timmar.
+# Programmet ska ha en output i form av en av följande textrader: "Enkelbiljett", "24-timmarsbiljett", "72-timmarsbiljett", "7-dagarsbiljett", "30-dagarsbiljett"
