c836760905a0f932fb13e5eb30a50a4909af8822
AntonSvenssonSpira <141945044+AntonSvenssonSpira@users.noreply.github.com>
2023-11-22 15:57:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
 def collatz_antal_steg(n):
+  #Skriv din kod här
   return None
 
 def collatz_lista(n):
+  #Skriv din kod här
   return None
+
+if __name__ == "__main__":
+  #Skriv din testkod här
+  None
