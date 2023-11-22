904e6e0d96413570d14a2d841fff886ee8c8c490
AntonSvenssonSpira <141945044+AntonSvenssonSpira@users.noreply.github.com>
2023-11-22 14:36:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-# Skriv ett program som rekommenderar vilken SL biljett användaren ska köpa.
-# Programmet ska ha 2 inputs. Input 1 hur många gånger användaren ska åka. Input 2 under hur long period sak användaren göra resorna i timmar.
-# Programmet ska ha en output i form av en av följande textrader: "Enkelbiljett", "24-timmarsbiljett", "72-timmarsbiljett", "7-dagarsbiljett", "30-dagarsbiljett"
+
+def collatz_antal_steg(n):
+  return None
+
+def collatz_lista(n):
+  return None
